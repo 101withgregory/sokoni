18bb11790f589487b350e2aafc6bdecf07a1a147
101withgregory <101withgregoryonyando@gmail.com>
2025-06-09 22:44:58 +0300

    fixed cors

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 4000
 await connectDB()
 await connectCloudinary()
 //allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://sokoni-client.vercel.app/']
+const allowedOrigins = ['http://localhost:5173', '']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 ///middleware configuration
